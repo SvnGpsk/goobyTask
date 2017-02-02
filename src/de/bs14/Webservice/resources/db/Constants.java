9e7a4e823fb1050459d94ebd9642646a4f0a5348
SvnGpsk <SvnGpsk@users.noreply.github.com>
2017-02-02 11:34:35 +0100

    changed database name

--- src/de/bs14/Webservice/resources/db/Constants.java
+++ src/de/bs14/Webservice/resources/db/Constants.java
@@ -3,11 +3,11 @@ package de.bs14.Webservice.resources.db;
 /**
  * 
  * @author it4-gapssv
- *	Konstanten für die Verbindung zu Datenbank.
+ *	Konstanten fÃ¼r die Verbindung zu Datenbank.
  */
 public class Constants {
   public static String dbClass = "com.mysql.jdbc.Driver";
-  private static String dbName= "users";
+  private static String dbName= "goobytask";
   public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
   public static String dbUser = "root";
   public static String dbPwd = "";
